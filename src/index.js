9c35cbd887b81aa56ec87b99d174d738e031cea4
Hassan Mekhamer <hassan.mekhamer.97@gmail.com>
2025-01-09 21:55:09 +0200

    Home page done with no styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import { menu } from "./menu";
 import "./style.css"
 
+import { appendFn } from "./home"
+
+//document selectors
+const content = document.querySelector("#content");
+const homeBtn = document.querySelector(".homeBtn");
+const menuBtn = document.querySelector(".menuBtn");
+const aboutBtn = document.querySelector(".aboutBtn");
+
+homeBtn.addEventListener("click", appendFn)
 
 
 
